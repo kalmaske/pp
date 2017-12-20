03852bd7eede112f305a36b3bb91d89921d69669
kalmaske <kalmaske@uncc.edu>
2017-12-20 14:07:31 -0500

    resume fix

---
+++ KhadijaAliResume/config.js
@@ -0,0 +1,1 @@
+IDRViewer.config = {"pagecount":2,"title":"","author":"Almiskeen, Ali","fileName":"KhadijaAliResume.pdf","bounds":[[935,1210],[935,1210]],"bookmarks":[],"thumbnailType":"jpg","pageType":"html","pageLabels":[]};
